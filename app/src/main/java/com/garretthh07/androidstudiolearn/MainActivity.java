10f73d7197f78a4d5a371a0ecd678d4cbdfb09ca
Garretthh07 <garretthh07@gmail.com>
2015-11-10 15:45:58 +0800

    Add Login Page To dev

--- app/src/main/java/com/garretthh07/androidstudiolearn/MainActivity.java
+++ app/src/main/java/com/garretthh07/androidstudiolearn/MainActivity.java
@@ -1,13 +1,14 @@
 package com.garretthh07.androidstudiolearn;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "This is just a test!1", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
                 System.out.println("This is just see the log");
+
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
             }
         });
     }
