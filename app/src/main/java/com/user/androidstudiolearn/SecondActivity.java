5f929831c1cdad397ea276c500d223355c87f43a
Garretthh07 <garretthh07@gmail.com>
2015-11-20 20:12:17 +0800

    add new activity

---
+++ app/src/main/java/com/user/androidstudiolearn/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.user.androidstudiolearn;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2015/11/20.
+ */  
+public class SecondActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
