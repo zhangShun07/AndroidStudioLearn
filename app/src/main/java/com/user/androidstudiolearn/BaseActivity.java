f8257bec3f0ce2e205ced6848b29c05270e23998
Garretthh07 <garretthh07@gmail.com>
2015-11-22 20:30:54 +0800

    for get current activity name

---
+++ app/src/main/java/com/user/androidstudiolearn/BaseActivity.java
@@ -0,0 +1,15 @@
+package com.user.androidstudiolearn;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class BaseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        Log.d("BaseActivity", getClass().getSimpleName());
+    }
+}
